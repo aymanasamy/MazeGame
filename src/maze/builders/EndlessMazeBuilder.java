1bb6368d33841400f374fa6d33e47305d2e0b8d2
ahm7 <elpopahmed462@gmail.com>
2017-12-16 18:38:59 +0400

    generation algorithm and endless maze class done

--- src/maze/builders/EndlessMazeBuilder.java
+++ src/maze/builders/EndlessMazeBuilder.java
@@ -1,15 +1,75 @@
 package maze.builders;
 
+import java.awt.Point;
+import java.util.Stack;
+
+import maze.IMazeElement;
 import maze.Maze;
+import maze.mazeElements.bombs.IBombFactory;
+import maze.mazeElements.bombs.RandomBombFactory;
+import maze.mazeElements.gates.EndGate;
+import maze.mazeElements.gates.IGate;
+import maze.mazeElements.gates.StartGate;
+import maze.mazeElements.gifts.IGiftFactory;
+import maze.mazeElements.gifts.RandomGiftFactory;
+import maze.mazeElements.monsters.IMonsterFactory;
+import maze.mazeElements.monsters.RandomMonsterFactory;
+import maze.mazeElements.walls.IWallFactory;
+import maze.mazeElements.walls.RandomWallFactory;
 
 public class EndlessMazeBuilder implements IMazeBuilder {
-    @Override
-    public Maze generate(int level, Difficulty difficulty) {
-        return null;
-    }
 
-    @Override
-    public IMazeBuilder getInstance() {
-        return null;
-    }
+	@Override
+	public Maze generate(int level, Difficulty difficulty) {
+		final int size = level * 3;
+		Stack<Point> myStack = new Stack();
+		MazeGenerationAlgorithm MyEndlessMazeinchractars = new MazeGenerationAlgorithm();
+		myStack = MyEndlessMazeinchractars.GetmazeInStack(size, size);
+		char[][] theMaze = MyEndlessMazeinchractars.GetMazeInCharacters(myStack, size, size);
+		Maze MyEndlessMaze = new Maze(theMaze.length, theMaze[0].length);
+		for (int i = 0; i < theMaze.length; i++) {
+			for (int j = 0; j < theMaze[0].length; j++) {
+				Point myElementPosition = new Point(i, j);
+				if (theMaze[i][j] == 'B') {
+					IBombFactory myboomb = RandomBombFactory.getInstance();
+					MyEndlessMaze.setElement(myElementPosition, myboomb.generate(MyEndlessMaze));
+
+				}
+				if (theMaze[i][j] == 'G') {
+					IGiftFactory mygift = RandomGiftFactory.getInstance();
+					MyEndlessMaze.setElement(myElementPosition, mygift.generate(MyEndlessMaze));
+
+				}
+
+				if (theMaze[i][j] == 'M') {
+					IMonsterFactory myMonister = RandomMonsterFactory.getInstance();
+					MyEndlessMaze.setElement(myElementPosition, myMonister.generate(MyEndlessMaze, 5));
+				}
+				if (theMaze[i][j] == 'S') {
+					IGate mystart = new StartGate();
+					MyEndlessMaze.setElement(myElementPosition, mystart);
+				}
+				if (theMaze[i][j] == 'E') {
+					IGate myEnd = new EndGate();
+					MyEndlessMaze.setElement(myElementPosition, myEnd);
+				}
+				if (theMaze[i][j] == '#') {
+					IWallFactory mywall = RandomWallFactory.getInstance();
+					MyEndlessMaze.setElement(myElementPosition, mywall.generate(MyEndlessMaze, false, 500));
+				}
+				if (theMaze[i][j] == 'T') {
+					IWallFactory myTree = RandomWallFactory.getInstance();
+					MyEndlessMaze.setElement(myElementPosition, myTree.generate(MyEndlessMaze, true,1));
+				}
+
+			}
+		}
+
+		return null;
+	}
+
+	@Override
+	public IMazeBuilder getInstance() {
+		return null;
+	}
 }
---
+++ src/maze/builders/MazeGenerationAlgorithm.java
@@ -0,0 +1,489 @@
+package maze.builders;
+
+import java.awt.Point;
+import java.util.Random;
+import java.util.Stack;
+
+public class MazeGenerationAlgorithm {
+	public static Stack<Point> myWay = new Stack();
+	public static Stack<Point> planb = new Stack();
+
+	private static char[][] maze;
+	static boolean[][] visited;
+
+	public Stack<Point> GetmazeInStack(int width, int height) {
+		int ObserveEnd = 0;
+		maze = new char[width][height];
+		visited = new boolean[width][height];
+		for (int i = 0; i < maze[0].length; i++) {
+			for (int j = 0; j < maze.length; j++) {
+
+				maze[i][j] = '#';
+			}
+		}
+
+		Random rand = new Random();
+
+		int n = rand.nextInt(maze.length);
+		int k = 0;
+		int l = 0;
+
+		Point CurrentPosition = new Point(n, 0);
+		Point PositionBeforeEdit = new Point(n, 0);
+		maze[n][0] = 'S';
+		visited[n][0] = true;
+		Point temp2 = new Point(0, 0);
+		temp2.x = CurrentPosition.x;
+		temp2.y = CurrentPosition.y;
+		myWay.push(temp2);
+		planb.push(temp2);
+		// System.out.println(temp);
+		while (true) {
+
+			if (CurrentPosition.x == maze.length || CurrentPosition.y == maze.length) {
+				break;
+			}
+
+			CurrentPosition = Search(CurrentPosition);
+
+			if (checkAround(CurrentPosition)) {
+
+				// System.out.println("fuck"+CurrentPosition);
+
+				int checkoutIfThereExistUnvisitedCell = 0;
+				int w = 0;
+				int z = 0;
+				for (w = 0; w < visited.length; w++) {
+					for (z = 0; z < visited[0].length; z++) {
+						if (!visited[w][z]) {
+							// System.out.println(w+" "+z);
+							checkoutIfThereExistUnvisitedCell = 1;
+							break;
+						}
+
+					}
+
+					if (checkoutIfThereExistUnvisitedCell == 1) {
+
+						int i = planb.size() - 1;
+						while (i >= 0) {
+							Point test1 = new Point(0, 1);
+							if (w == 0 && z == 0) {
+								if (myWay.contains(test1)) {
+									Point temp5 = new Point(test1.x, test1.y);
+									Point temp6 = new Point(w, z);
+									visited[w][z] = true;
+									maze[w][z] = 'F';
+									myWay.push(temp5);
+									myWay.push(temp6);
+								}
+								Point test5 = new Point(1, 0);
+								if (myWay.contains(test5)) {
+									Point temp5 = new Point(test5.x, test5.y);
+									Point temp6 = new Point(w, z);
+									visited[w][z] = true;
+									maze[w][z] = 'F';
+									myWay.push(temp5);
+									myWay.push(temp6);
+								}
+
+							}
+							// System.out.println(myWay.size()+"
+							// "+planb.size());
+							test1 = planb.pop();
+							// System.out.println(test1+"test");
+							if (!checkAround(test1)) {
+
+								Point test = new Point(0, 0);
+								test.x = test1.x;
+								test.y = test1.y;
+								Point temp3 = new Point(test.x, test.y);
+								myWay.push(test1);
+								// System.out.println(test + "Sds");
+
+								CurrentPosition.x = test.x;
+								CurrentPosition.y = test.y;
+
+								break;
+							}
+							i--;
+						}
+						planb.clear();
+						planb.addAll(myWay);
+
+						break;
+					}
+					// System.out.println(" ");
+				}
+
+				if (checkoutIfThereExistUnvisitedCell == 0) {
+					break;
+				}
+			} else {
+				PositionBeforeEdit = CurrentPosition;
+			}
+
+			/*
+			 * for (int h = 0; h < maze.length; h++) { for (int u = 0; u
+			 * <maze.length; u++) { System.out.print(maze[h][u]); }
+			 * System.out.println(" "); }
+			 * System.out.println("-------------------------------");
+			 */
+		}
+
+		int size = myWay.size();
+		/*
+		 * for (int i = 0; i < size; i++) { System.out.println(myWay.get(i));
+		 * 
+		 * }
+		 */
+
+		return myWay;
+	}
+
+	private static Point SearchRight(Point currentPosition) {
+
+		int x = currentPosition.x;
+		int y = currentPosition.y;
+		// System.out.println(x+" "+y);
+		if (y >= maze.length || x >= maze.length || y + 1 >= maze[0].length || visited[x][y + 1]) {
+			return currentPosition;
+		}
+		// System.out.println("right");
+		visited[x][y + 1] = true;
+
+		currentPosition.y = y + 1;
+		maze[x][y + 1] = 'F';
+		Point temp = new Point(0, 0);
+		temp.x = currentPosition.x;
+		temp.y = currentPosition.y;
+		myWay.push(temp);
+		planb.push(temp);
+		// System.out.println(temp);
+		// TODO Auto-generated method stub
+		return currentPosition;
+	}
+
+	private static Point SearchLeft(Point currentPosition) {
+		// System.out.println("sfdfs");
+		int x = currentPosition.x;
+		int y = currentPosition.y;
+		// System.out.println(x+" "+y);
+		// System.out.println("sfdfs");
+
+		if (y >= maze.length || x >= maze.length || y - 1 < 0 || visited[x][y - 1]) {
+			return currentPosition;
+		}
+		// System.out.println("left");
+
+		visited[x][y - 1] = true;
+		maze[x][y - 1] = 'F';
+
+		currentPosition.y = y - 1;
+		Point temp = new Point(0, 0);
+		temp.x = currentPosition.x;
+		temp.y = currentPosition.y;
+		myWay.push(temp);
+		planb.push(temp);
+		// System.out.println(temp);
+		// TODO Auto-generated method stub
+		return currentPosition;
+	}
+
+	private static Point SearchDown(Point currentPosition) {
+		int x = currentPosition.x;
+		int y = currentPosition.y;
+		// System.out.println(x+" "+y);
+
+		if (y >= maze.length || x >= maze.length || x + 1 >= maze.length || visited[x + 1][y]) {
+			return currentPosition;
+		}
+		// System.out.println("down");
+
+		visited[x + 1][y] = true;
+
+		currentPosition.x = x + 1;
+		maze[x + 1][y] = 'F';
+		Point temp = new Point(0, 0);
+		temp.x = currentPosition.x;
+		temp.y = currentPosition.y;
+		myWay.push(temp);
+		planb.push(temp);
+		// TODO Auto-generated method stub
+		return currentPosition;
+	}
+
+	private static Point SearchUp(Point currentPosition) {
+		int x = currentPosition.x;
+		int y = currentPosition.y;
+		// System.out.println(x+" "+y);
+
+		if (y >= maze.length || x >= maze.length || x - 1 < 0 || visited[x - 1][y]) {
+			return currentPosition;
+		}
+		// System.out.println("up");
+
+		visited[x - 1][y] = true;
+		maze[x - 1][y] = 'F';
+
+		currentPosition.x = x - 1;
+		Point temp = new Point(0, 0);
+		temp.x = currentPosition.x;
+		temp.y = currentPosition.y;
+		myWay.push(temp);
+		planb.push(temp);
+		// System.out.println(temp);
+
+		// TODO Auto-generated method stub
+		return currentPosition;
+	}
+
+	public static boolean checkAround(Point mypoint) {
+
+		if ((mypoint.x) - 1 > 0) {
+
+			if (!visited[(mypoint.x) - 1][mypoint.y]) {
+				return false;
+			}
+		}
+		if ((mypoint.x) + 1 < maze.length) {
+
+			if (!visited[(mypoint.x) + 1][mypoint.y]) {
+				return false;
+			}
+		}
+		if ((mypoint.y) - 1 > 0) {
+
+			if (!visited[mypoint.x][(mypoint.y) - 1]) {
+				return false;
+			}
+		}
+		if ((mypoint.y) + 1 < maze.length) {
+
+			if (!visited[mypoint.x][(mypoint.y) + 1]) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public static Point Search(Point CurrentPosition) {
+		Random rand = new Random();
+		int Direction = rand.nextInt(4) + 1;
+		Point temp = new Point(0, 0);
+		if (Direction == 1) {
+			temp = SearchUp(CurrentPosition);
+			if (temp == CurrentPosition) {
+				Direction = rand.nextInt(3) + 2;
+			}
+		}
+		if (Direction == 2) {
+			temp = SearchDown(CurrentPosition);
+			if (temp == CurrentPosition) {
+				Direction = rand.nextInt(2) + 3;
+			}
+		}
+		if (Direction == 3) {
+			temp = SearchRight(CurrentPosition);
+			if (temp == CurrentPosition) {
+				Direction = 4;
+			}
+		}
+		if (Direction == 4) {
+			temp = SearchLeft(CurrentPosition);
+
+		}
+
+		return temp;
+
+	}
+
+	public char[][] GetMazeInCharacters(Stack<Point> myStack, int width, int height) {
+		Stack<Point> newStack = new Stack();
+		char[][] MyCompleteMaze = new char[width * 2 + 1][height * 2 + 1];
+		final int NumOfGifts = MyCompleteMaze.length / 10;
+		final int NumOfMonisters = MyCompleteMaze.length / 10;
+		for (int x = 0; x < MyCompleteMaze.length; x++) {
+			for (int y = 0; y < MyCompleteMaze[0].length; y++) {
+				MyCompleteMaze[x][y] = '#';
+			}
+		}
+		int order = 1;
+		for (int j = 0; j < myStack.size(); j++) {
+			for (int i = 0; i < myStack.size(); i++) {
+				int x = myStack.get(i).x;
+				int y = myStack.get(i).y;
+
+				/* take a random end of the maze */
+				if (order == 1) {
+					Random rand = new Random();
+					int end = rand.nextInt(maze.length);
+					maze[end][maze.length - 1] = 'E';
+					order++;
+				}
+				/* end thid task */
+
+				x = x + x + 1;
+				y = y + y + 1;
+
+				Point newPoint = new Point(x, y);
+				newStack.push(newPoint);
+				if (maze[myStack.get(i).x][myStack.get(i).y] == 'S') {
+					MyCompleteMaze[x][y] = 'S';
+				} else if (maze[myStack.get(i).x][myStack.get(i).y] == 'E') {
+					MyCompleteMaze[x][y] = 'E';
+				} else {
+					MyCompleteMaze[x][y] = 'f';
+				}
+			}
+			order++;
+
+		}
+
+		for (int f = 0; f < newStack.size() - 1; f++) {
+			int x = newStack.get(f).x;
+			int y = newStack.get(f).y;
+			int x2 = newStack.get(f + 1).x;
+			int y2 = newStack.get(f + 1).y;
+
+			if (x == (x2 + 2) && y == y2) {
+				MyCompleteMaze[x - 1][y] = 'f';
+
+			} else if ((x + 2) == x2 && y == y2) {
+				MyCompleteMaze[x2 - 1][y] = 'f';
+			} else if (y == (y2 + 2) && x == x2) {
+				MyCompleteMaze[x][y - 1] = 'f';
+
+			} else if ((y + 2) == y2 && x == x2) {
+				MyCompleteMaze[x][y2 - 1] = 'f';
+			}
+
+		}
+
+		for (int x = 0; x < MyCompleteMaze.length; x++) {
+			for (int y = 0; y < MyCompleteMaze[0].length; y++) {
+				if (MyCompleteMaze[x][y] == 'E' && y == (MyCompleteMaze.length) - 2) {
+					MyCompleteMaze[x][y + 1] = 'E';
+					MyCompleteMaze[x][y] = 'f';
+				}
+				if (MyCompleteMaze[x][y] == 'S' && y == 1) {
+					MyCompleteMaze[x][y - 1] = 'S';
+					MyCompleteMaze[x][y] = 'f';
+				}
+			}
+		}
+
+		MyCompleteMaze = putBooms(MyCompleteMaze, 0);
+		MyCompleteMaze = putGifts(MyCompleteMaze, 0);
+		MyCompleteMaze = putMonisters(MyCompleteMaze, 0);
+
+		return MyCompleteMaze;
+	}
+	
+	
+	public char [][] putBooms(char [][]MyCompleteMaze,int difficulty){
+		final int NumOfBooms = MyCompleteMaze.length / 2;
+		int i=0;
+		while(i<NumOfBooms) {
+
+			Random rand = new Random();
+			int x = rand.nextInt(maze.length);
+			int y = rand.nextInt(maze.length);
+			
+			if(MyCompleteMaze[x][y]== 'S' ||
+					MyCompleteMaze[x][y]== 'E'|| 
+					MyCompleteMaze[x][y]=='B' ||
+					MyCompleteMaze[x][y]=='M' ||
+					MyCompleteMaze[x][y]=='G'||
+				    MyCompleteMaze[x][y]=='T'||
+					x==0 || y==0){
+				 
+			}else{
+				MyCompleteMaze[x][y]='B';
+				i++;
+			}
+		}
+		return MyCompleteMaze;
+	}
+	public char [][] putMonisters(char [][]MyCompleteMaze,int difficulty){
+		final int NumOfMonisters = MyCompleteMaze.length / 2;
+		int i=0;
+		while(i<NumOfMonisters) {
+
+			Random rand = new Random();
+			int x = rand.nextInt(maze.length);
+			int y = rand.nextInt(maze.length);
+			
+			if(MyCompleteMaze[x][y]== 'S' ||
+					MyCompleteMaze[x][y]== 'E'|| 
+					MyCompleteMaze[x][y]=='B' ||
+					MyCompleteMaze[x][y]=='M' ||
+					MyCompleteMaze[x][y]=='G'||
+				    MyCompleteMaze[x][y]=='T'||
+					x==0 || y==0){
+				 
+			}else{
+				MyCompleteMaze[x][y]='M';
+				i++;
+			}
+		}
+		
+		return MyCompleteMaze;
+	}
+	public char [][] putGifts(char [][]MyCompleteMaze,int difficulty){
+		final int NumOfGifts = MyCompleteMaze.length / 2;
+		int i=0;
+		while(i<NumOfGifts) {
+
+			Random rand = new Random();
+			int x = rand.nextInt(maze.length);
+			int y = rand.nextInt(maze.length);
+			
+			if(MyCompleteMaze[x][y]== 'S' ||
+					MyCompleteMaze[x][y]== 'E'|| 
+					MyCompleteMaze[x][y]=='B' ||
+					MyCompleteMaze[x][y]=='M' ||
+					MyCompleteMaze[x][y]=='G'||
+				    MyCompleteMaze[x][y]=='T'||
+					x==0 || y==0){
+				 
+			}else{
+				MyCompleteMaze[x][y]='G';
+				i++;
+			}
+		}
+		return MyCompleteMaze;
+		
+	}
+	public char [][] puttrees(char [][]MyCompleteMaze,int difficulty){
+		final int NumOftrees = MyCompleteMaze.length / 2;
+		int i=0;
+		while(i<NumOftrees) {
+
+			Random rand = new Random();
+			int x = rand.nextInt(maze.length);
+			int y = rand.nextInt(maze.length);
+			
+		if(MyCompleteMaze[x][y]== 'S' ||
+				MyCompleteMaze[x][y]== 'E'|| 
+				MyCompleteMaze[x][y]=='B' ||
+				MyCompleteMaze[x][y]=='M' ||
+				MyCompleteMaze[x][y]=='G'||
+			    MyCompleteMaze[x][y]=='T'||
+				x==0 || y==0){
+				 
+			}else{
+				MyCompleteMaze[x][y]='T';
+				i++;
+			}
+		}
+		return MyCompleteMaze;
+		
+	}
+	
+	
+	
+	
+
+}
