4f42d4bc6abb85694cb75e82bbc523c6ebba72de
Ayman <ayman.a.samy.m@gmail.com>
2017-12-19 11:56:30 +0200

    Saver and RoundsMazeBuilder need to be implemented

--- src/maze/builders/RoundsMazeBuilder.java
+++ src/maze/builders/RoundsMazeBuilder.java
@@ -5,6 +5,9 @@ import maze.Maze;
 public class RoundsMazeBuilder implements IMazeBuilder {
     @Override
     public Maze generate(int level, Difficulty difficulty) {
+        // Check for the maze if saved
+        // if saved : load it
+        // else : Build it using EndlessMazeBuilder then save it
         return null;
     }
 
---
+++ src/maze/saving/Saver.java
@@ -0,0 +1,18 @@
+package maze.saving;
+
+import maze.Maze;
+import maze.builders.Difficulty;
+
+public class Saver {
+    void save(Difficulty difficulty,int level,Maze maze) {
+        // Todo
+    }
+    Maze load(Difficulty difficulty,int level) {
+        // Todo
+        return null;
+    }
+    boolean isSaved(Difficulty difficulty,int level){
+        // Todo
+        return false;
+    }
+}
