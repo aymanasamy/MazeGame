622d2f5e731aa2efadc54c3539018111a22cd17a
Ayman <ayman.a.samy.m@gmail.com>
2017-12-19 12:14:18 +0200

    Added getMazeRunner to Maze and some editing in maze

--- src/maze/Maze.java
+++ src/maze/Maze.java
@@ -2,6 +2,7 @@ package maze;
 
 import maze.helpingclasses.PointCompare;
 import maze.mazeElements.EmptyElement;
+import maze.mazeElements.mazeRunner.MazeRunner;
 import maze.mazeElements.monsters.IMonster;
 
 import java.awt.*;
@@ -9,43 +10,46 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Maze {
-    public int getWidth() {
-        return width;
+    private int width;
+    private int height;
+    private IMazeElement[][] map;
+
+    public Maze(int width, int height) {
+        this.width = width;
+        this.height = height;
+        map = new IMazeElement[width][height];
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                map[i][j] = new EmptyElement();
+            }
+        }
     }
+    public MazeRunner getMazeRunner(){
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if (map[i][j] instanceof MazeRunner) {
+                    return (MazeRunner) map[i][j];
+                }
+            }
+        }
+        return null;
+    }
+    public int getWidth() { return width; }
 
     public int getHeight() {
         return height;
     }
 
-    private int width;
-    private int height;
-    private IMazeElement[][] map;
-
-    public Maze(int width,int height) {
-        this.width = width;
-        this.height = height;
-        map = new IMazeElement[width][height];
-        for (int i = 0;i < width;i++) {
-            for (int j = 0;j < height;j++) {
-                map[i][j] = new EmptyElement();
-            }
-        }
-    }
-
     public IMazeElement getElement(Point position) {
-        if(isNotInTheMaze(position))
+        if (isNotInTheMaze(position))
             return null;
         return map[position.x][position.y];
     }
 
-    private boolean isNotInTheMaze(Point position) {
-        return position.x >= width || position.x < 0 || position.y < 0 || position.y >= height;
-    }
-
     public void removeElement(IMazeElement element) {
-        for (int i = 0;i < width;i++) {
+        for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
-                if(element == map[i][j]) {
+                if (element == map[i][j]) {
                     map[i][j] = new EmptyElement();
                 }
             }
@@ -53,55 +57,61 @@ public class Maze {
     }
 
     public boolean setElement(Point position, IMazeElement element) {
-        if(elementIsNotInTheMaze(position, element))
+        if (elementIsNotInTheMaze(position, element))
             return false;
         // Affect all elements in this place
-        for(int i = position.x;i < position.x + element.getWidth();i++) {
-            for(int j = position.y;j < position.y + element.getHeight();j++) {
+        for (int i = position.x; i < position.x + element.getWidth(); i++) {
+            for (int j = position.y; j < position.y + element.getHeight(); j++) {
                 element.affect(map[i][j]);
                 map[i][j].affect(element);
             }
         }
-
-        boolean hasFreeSpace = true;
-        for(int i = position.x;i < position.x + element.getWidth();i++) {
-            for(int j = position.y;j < position.y + element.getHeight();j++) {
-                if(!(map[i][j] instanceof EmptyElement)) {
-                    hasFreeSpace = false;
-                    break;
-                }
-            }
-        }
-        if(!hasFreeSpace) {
+        if (!hasFreeSpace(position, element))
             return false;
-        }
-
-        for(int i = position.x;i < position.x + element.getWidth();i++) {
-            for(int j = position.y;j < position.y + element.getHeight();j++) {
+        for (int i = position.x; i < position.x + element.getWidth(); i++) {
+            for (int j = position.y; j < position.y + element.getHeight(); j++) {
                 map[i][j] = element;
             }
         }
         return true;
     }
 
-    private boolean elementIsNotInTheMaze(Point position, IMazeElement element) {
-        return isNotInTheMaze(position) || isNotInTheMaze(new Point(position.x + element.getWidth(),position.y + element.getHeight()));
-    }
     public Point getPosition(IMazeElement element) {
         // Add all the points that have this element
         ArrayList<Point> points = new ArrayList<>();
-        for (int i = 0;i < width;i++) {
+        for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
-                if(element == map[i][j]) {
-                    points.add(new Point(i,j));
+                if (element == map[i][j]) {
+                    points.add(new Point(i, j));
                 }
             }
         }
         // Return the smallest one
         Collections.sort(points, new PointCompare());
-        if(points.size() == 0)
+        if (points.size() == 0)
             return null;
         else
             return points.get(0);
+    }
+
+    private boolean hasFreeSpace(Point position, IMazeElement element) {
+        boolean hasFreeSpace = true;
+        for (int i = position.x; i < position.x + element.getWidth(); i++) {
+            for (int j = position.y; j < position.y + element.getHeight(); j++) {
+                if (!(map[i][j] instanceof EmptyElement)) {
+                    hasFreeSpace = false;
+                    break;
+                }
+            }
+        }
+        return hasFreeSpace;
+    }
+
+    private boolean elementIsNotInTheMaze(Point position, IMazeElement element) {
+        return isNotInTheMaze(position) || isNotInTheMaze(new Point(position.x + element.getWidth(), position.y + element.getHeight()));
+    }
+
+    private boolean isNotInTheMaze(Point position) {
+        return position.x >= width || position.x < 0 || position.y < 0 || position.y >= height;
     }
 }
